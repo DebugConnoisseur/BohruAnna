2d4c71b0815dbea91c93a0dc2d607523996f097f
HeliumSpectrum <67976720+HeliumSpectrum@users.noreply.github.com>
2020-07-08 21:48:47 +0530

    Add files via upload

---
+++ bohrtrollinghimself.py
@@ -0,0 +1,19 @@
+import sys
+print(sys.executable)
+import discord
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+        if message.content.startswith('!assfellow'):
+            await message.channel.send('I am ass fellow and a dumb idiot {0.author.mention}'.format(message))
+
+client = MyClient()
+client.run('NzMwMDA5ODg1NDE2MzU3OTY4.XwXvdg.t0k_YQXdKLRfmkgSFFLvxQTqhTw')
+
